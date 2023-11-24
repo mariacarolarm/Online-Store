de85d8f6a9a8a03ebe558e4647a2f2bb98b774e2
Maria <marycarol.00@gmail.com>
2023-11-24 12:50:16 -0300

    [Grupo 06] Requisito 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/ShoppingCart" element={ <ShoppingCart /> } />
+      </Routes>
     </div>
   );
 }
