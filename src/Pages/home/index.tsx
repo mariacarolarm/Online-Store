63a2d5ff238a64c9407d2c679e54467ecc5f2818
Mariana de Souza <desouzamariana.hb@gmail.com>
2023-11-25 10:18:14 -0300

    Requisito 04

--- src/Pages/home/index.tsx
+++ src/Pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import ListCategory from '../../components/ListCategory';
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <Link to="/ShoppingCart" data-testid="shopping-cart-button">
         <button>Ir para o Carrinho</button>
       </Link>
+      <ListCategory />
     </>
   );
 }
