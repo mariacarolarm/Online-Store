92a09ca27caeeb74cc9e1ec24653826e84fb7406
Luccas Santos <luccasmsantos@outlook.com>
2023-11-27 17:28:42 -0300

    Requisito 10 - Finalizado

--- src/components/ClientCart.tsx
+++ src/components/ClientCart.tsx
@@ -1,13 +1,46 @@
+import { useState, useEffect } from 'react';
 import { ProductCartType } from './types';
 
 type ProductCartProp = {
   product: ProductCartType,
+  onItemRemove: (productId: string) => void;
 };
 
-function ClientCart({ product }: ProductCartProp) {
+function ClientCart({ product, onItemRemove }: ProductCartProp) {
+  const [quantity, setQuantity] = useState(product.quantity);
+
+  useEffect(() => {
+    const currentCart: ProductCartType[] = JSON.parse(localStorage
+      .getItem('carrinho') || '[]');
+
+    const updatedCart = currentCart
+      .map((item) => (item.id === product.id ? { ...item, quantity } : item));
+    localStorage.setItem('carrinho', JSON.stringify(updatedCart));
+  }, [quantity, product.id]);
+
+  const updateLocalStorage = () => {
+    const newLocalStorage = JSON.parse(localStorage.getItem('carrinho') || '[]')
+      .filter((item: ProductCartType) => item.id !== product.id);
+    localStorage.setItem('carrinho', JSON.stringify(newLocalStorage));
+    onItemRemove(product.id);
+  };
+
+  const addQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+  const subQuantity = () => {
+    setQuantity((prevQuantity) => Math.max(prevQuantity - 1, 1));
+  };
   return (
     <div>
       <p data-testid="shopping-cart-product-name">
+        <button
+          onClick={ updateLocalStorage }
+          type="button"
+          data-testid="remove-product"
+        >
+          X
+        </button>
         Nome do Produto:
         {' '}
         {product.title}
@@ -15,16 +48,26 @@ function ClientCart({ product }: ProductCartProp) {
       {product.thumbnail && (
         <img
           src={ product.thumbnail }
-          alt={ product.thumbnail }
+          alt={ `Thumbnail of ${product.title}` }
         />
       )}
-      <p data-testid="shopping-cart-product-quantity">
-        Quantidade:
-        {' '}
-        {product.quantity}
-      </p>
+      <p>Quantidade:</p>
+      <button
+        type="button"
+        onClick={ subQuantity }
+        data-testid="product-decrease-quantity"
+      >
+        -
+      </button>
+      <span data-testid="shopping-cart-product-quantity">{quantity}</span>
+      <button
+        type="button"
+        onClick={ addQuantity }
+        data-testid="product-increase-quantity"
+      >
+        +
+      </button>
     </div>
   );
 }
-
 export default ClientCart;
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import ClientCart from './ClientCart';
 import { ProductCartType } from './types';
 
 function ShoppingCart() {
-  const getLocalStorage = JSON.parse(localStorage.getItem('carrinho') || '[]');
+  const [cartItems, setCartItems] = useState<ProductCartType[]>(
+    JSON.parse(localStorage.getItem('carrinho') || '[]'),
+  );
+
+  const handleItemRemove = (productId: string) => {
+    const updatedCart = cartItems.filter((item) => item.id !== productId);
+    setCartItems(updatedCart);
+    localStorage.setItem('carrinho', JSON.stringify(updatedCart));
+  };
 
   return (
     <div>
-      {
-        getLocalStorage.length > 0 ? getLocalStorage.map((product: ProductCartType) => (
+      {cartItems.length > 0 ? (
+        cartItems.map((product) => (
           <div key={ product.id }>
             <ClientCart
               product={ product }
+              onItemRemove={ handleItemRemove } // Passando a função de remoção
             />
           </div>
-        )) : (
-          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-        )
-      }
+        ))
+      ) : (
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      )}
     </div>
   );
 }
